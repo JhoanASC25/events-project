a2d021f16848a5b5d281cd9eb2e26b53a3f3943c
JhoanASC25 <jhoanalonso53@gmail.com>
2025-07-28 16:39:00 -0400

    yayayaya

---
+++ script.js
@@ -0,0 +1,55 @@
+let box = document.querySelector("div");
+/*
+let aquaButton = document.querySelector("#aqua")
+let tealButton = document.querySelector("#teal")
+let pinkButton = document.querySelector("#pink")
+let purpleButton = document.querySelector("#purple")
+box.style.backgroundColor = 'white'
+
+aquaButton.onclick = function(){
+    if(box.style.backgroundColor == 'white'){
+        box.style.backgroundColor = 'aqua';
+        box.innerHTML = 'aqua'
+    }
+}
+*/
+
+let aquaButton = document.querySelector("#aqua")
+let tealButton = document.querySelector("#teal")
+let pinkButton = document.querySelector("#pink")
+let purpleButton = document.querySelector("#purple")
+let colorlessButton = document.querySelector('#colorless')
+
+aquaButton.onclick = function(){
+    if(1 == 1){
+        box.style.backgroundColor = 'aqua';
+        box.innerHTML = 'aqua'
+    }
+}
+tealButton.onclick = function(){
+    if(1 == 1){
+        box.style.backgroundColor = 'teal';
+        box.innerHTML = 'teal'
+    }
+}
+
+pinkButton.onclick = function(){
+    if(1 == 1){
+        box.style.backgroundColor = 'pink';
+        box.innerHTML = 'pink'
+    }
+}
+
+purpleButton.onclick = function(){
+    if(1 == 1){
+        box.style.backgroundColor = 'purple';
+        box.innerHTML = 'purple'
+    }
+}
+
+colorlessButton.onclick = function(){
+    if(1 == 1){
+        box.style.backgroundColor = 'white';
+        box.innerHTML = 'colorless'
+    }
+}
